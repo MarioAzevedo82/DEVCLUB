8350ebb21ff0663f39d1ce659c9e49d9595b9ac9
Mario Azevedo <mariobct@msn.com>
2026-02-04 11:58:04 -0300

    1 , 2 e 3

--- exercicios.js
+++ exercicios.js
@@ -1,1 +1,20 @@
 /* Módulo 1 EXERCÍCIOS */
+
+console.log("Olá, mundo!")
+
+a = 1
+b = 2
+c = 3
+
+media = ((a + b + c)/3)
+
+console.log(media)
+
+n = 8
+
+if(n > 10){
+    console.log(`n é maior q 10`)
+} else {
+    console.log("n é menor q 10")
+}
+
