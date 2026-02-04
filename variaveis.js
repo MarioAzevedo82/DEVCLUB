3aaba52bb33dcef08113c23d8f871d099dd3ba26
Mario Azevedo <mariobct@msn.com>
2026-02-04 12:35:50 -0300

    variáveis

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+/*
+Variáveis 
+
+-let -> Pode alterar o valor durante o código
+-const -> Valor imutável, não pode ser declarado novamente.
+-var -> DESCONTINUADO NÃO USAR
+
+*/
+
+const abacate = 40
+
+console.log(abacate)
