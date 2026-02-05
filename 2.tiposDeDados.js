00fd52a61f7931e5792aacd72190f37866a2ce00
Mario Azevedo <mariobct@msn.com>
2026-02-05 10:31:40 -0300

    Strings

---
+++ 2.tiposDeDados.js
@@ -0,0 +1,20 @@
+/*
+1. String - Textos
+ 
+*/
+
+const texto1 = "Textos com aspas duplas"
+const texto2 = 'Textos com aspas simples'
+const texto3 = `Textos com a crase` //Template String
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)
+
+const myAge = 43
+const wifeAge = 39
+const myString = `Minha idade é ${myAge} e da minha esposa é ${wifeAge}` 
+
+console.log(myString)
+
+
