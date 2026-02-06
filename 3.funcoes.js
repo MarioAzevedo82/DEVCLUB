d5225a155c55218483da243a2b46eff2e4d3a6ca
Mario Azevedo <mariobct@msn.com>
2026-02-06 10:14:49 -0300

    Funçoes introdução

---
+++ 3.funcoes.js
@@ -0,0 +1,14 @@
+/*
+Functions -> Funções
+    Trecho de código que é executado 
+    SOMENTE quando solicitado
+
+*/
+const variavel = "Abacate"
+console.log(variavel)
+
+function nomeNaTela() {
+    console.log("mario")
+}
+
+nomeNaTela() // Só aparece qndo ela for chamada
