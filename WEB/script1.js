1a2e29e70bdacc2286e528d4f775e4e71a91d745
Mario Azevedo <mariobct@msn.com>
2026-02-10 14:18:39 -0300

    addEventListener 2

--- WEB/script1.js
+++ WEB/script1.js
@@ -10,3 +10,15 @@ const button = document.querySelector(".main-button")
 select.addEventListener("change", function() {
     console.log("troquei o valor")
 })
+
+function troqueiValor(event){
+    console.log(event)
+}
+
+select.addEventListener("change", troqueiValor)
+
+input.addEventListener("keypress", troqueiValor)
+
+button.addEventListener("click", function(){
+    console.log("Clicou no verde")
+})
