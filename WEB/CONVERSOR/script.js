533592af03b79c01c8601291a0274f1a4d73e6c0
Mario Azevedo <mariobct@msn.com>
2026-02-22 11:39:34 -0300

    mostrar no html valores

--- WEB/CONVERSOR/script.js
+++ WEB/CONVERSOR/script.js
@@ -2,10 +2,15 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value") //Outras moedas
 
     const dolarToday = 5.2
 
-    const convertValue = inputCurrencyValue / dolarToday
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = inputCurrencyValue
+    currencyValueConverted.innerHTML = convertedValue
 
     console.log(convertValue)
 }
