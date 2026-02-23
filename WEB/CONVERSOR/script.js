7220bee3c35be35caf95740ca762caaa6b158e77
Mario Azevedo <mariobct@msn.com>
2026-02-23 09:58:13 -0300

    currencySelect

--- WEB/CONVERSOR/script.js
+++ WEB/CONVERSOR/script.js
@@ -1,11 +1,14 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em Real
     const currencyValueConverted = document.querySelector(".currency-value") //Outras moedas
 
+    console.log(currencySelect.value)
     const dolarToday = 5.2
+    const euroToday = 6.2
 
     const convertedValue = inputCurrencyValue / dolarToday
 
@@ -20,7 +23,7 @@ function convertValues() {
     }).format(convertedValue)
 
     
-    console.log(convertValue)
+    
 }
 
 
